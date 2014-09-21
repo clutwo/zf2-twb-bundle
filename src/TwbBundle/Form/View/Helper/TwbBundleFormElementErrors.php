8eec6f45f73b835988565d2455dba921651e887b
Clemens Luschnig <clemens.luschnig@gmail.com>
2014-09-21 12:10:06 +0200

    changed error view style

--- src/TwbBundle/Form/View/Helper/TwbBundleFormElementErrors.php
+++ src/TwbBundle/Form/View/Helper/TwbBundleFormElementErrors.php
@@ -1,7 +1,7 @@
 <?php
 namespace TwbBundle\Form\View\Helper;
 class TwbBundleFormElementErrors extends \Zend\Form\View\Helper\FormElementErrors{
-	protected $messageOpenFormat = '<ul class="help-block"><li>';
-	protected $messageCloseString = '</li></ul>';
-	protected $messageSeparatorString = '</li><li>';
+	protected $messageOpenFormat = '<div class="help-block"><span>';
+	protected $messageCloseString = '</span></div>';
+	protected $messageSeparatorString = '</span><span>';
 }
